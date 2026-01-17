78c9ae000a01139ac10f150d52988ecf9896a2ca
srmurali002 <srmurali002@gmail.com>
2026-01-17 22:20:10 +0530

    feat: implement Resize operator and add unit tests

--- jaxonnxruntime/__init__.py
+++ jaxonnxruntime/__init__.py
@@ -91,6 +91,7 @@ from jaxonnxruntime.onnx_ops import reducemean
 from jaxonnxruntime.onnx_ops import reducesum
 from jaxonnxruntime.onnx_ops import relu
 from jaxonnxruntime.onnx_ops import reshape
+from jaxonnxruntime.onnx_ops import resize
 from jaxonnxruntime.onnx_ops import scatterelements
 from jaxonnxruntime.onnx_ops import scatternd
 from jaxonnxruntime.onnx_ops import selu
